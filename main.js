239191bd759aa4faf8a7ea1e21cb824b3849cd4d
mckelveygreg <mckelveygreg@gmail.com>
2018-08-17 12:31:52 -0700

    axes passing tests

---
+++ main.js
@@ -0,0 +1,61 @@
+const root = document.getElementById('root');
+const url = "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/global-temperature.json";
+
+d3.json(url)
+    .then(data => buildGraph(data));
+
+const buildGraph = (data) => {
+    const dataset = data.monthlyVariance;
+    //console.log(dataset);
+    // Dimensions
+    const width = root.clientWidth;
+    const height = root.clientHeight;
+    const padding = 50;
+    
+    const svg = d3.select(root)
+                    .append('svg')
+                    .attr('width', width)
+                    .attr('height', height);
+    // Titles
+    svg.append('text')
+        .text('Monthly Global Land-Surface Temperature')
+        .attr('text-anchor', 'middle')
+        .attr('id', 'title')
+        .attr('font-size', '20')
+        .attr('transform', `translate(${width/2}, 50)`);
+    svg.append('text')
+        .text('1753 - 2015: base temperature 8.66℃')
+        .attr('id', 'description')
+        .attr('text-anchor', 'middle')
+        .attr('transform', `translate(${width/2}, 75)`);
+
+    // x-scale
+    const yearMax = d3.max(dataset, d => d.year);
+    const yearMin = d3.min(dataset, d => d.year);
+    const parseTimeYear = d3.timeParse('%Y');
+    const xScaleYear = d3.scaleTime()
+                            .domain([parseTimeYear(yearMin), parseTimeYear(yearMax)])
+                            .range([padding, width-padding])
+    const xAxis = d3.axisBottom(xScaleYear).tickFormat(d3.timeFormat('%Y'));
+
+    // Y-Scale, Months
+    const formatMonth = d3.timeFormat('%B');
+   
+    const yScaleMonth = d3.scaleTime()
+    .domain([new Date(2000, 0, 1, 0), new Date(2000, 11, 1, 0)])
+    .range([padding,height - padding])
+    //.ticks(d3.timeYear.every(1));;
+                        
+    const yAxis = d3.axisLeft(yScaleMonth)
+                    .tickFormat(formatMonth)
+                    
+    // Axes
+    svg.append('g')
+        .attr('transform', `translate(0,${height - padding})`)
+        .property('id', 'x-axis')
+        .call(xAxis);
+    svg.append('g')
+        .attr('transform', `translate(${padding}, 0)`)
+        .property('id', 'y-axis')
+        .call(yAxis);
+}
