fcb45e2acda8fa7b3faf4b452f350f11812c19e6
mckelveygreg <mckelveygreg@gmail.com>
2018-08-19 11:17:08 -0700

    padding object

--- main.js
+++ main.js
@@ -8,38 +8,41 @@ const buildGraph = (data) => {
     const dataset = data.monthlyVariance;
     //console.log(dataset);
     // Dimensions
-    const width = root.clientWidth;
-    const height = root.clientHeight;
-    const padding = 50;
+    const padding = { top: 100, right: 10, bottom: 150, left: 80 }
+    console.log(padding);
+    const width = root.clientWidth - padding.right - padding.left;
+    const height = root.clientHeight - padding.top - padding.bottom;
     const yearMax = d3.max(dataset, d => d.year);
     const yearMin = d3.min(dataset, d => d.year);
     const tempMax = d3.max(dataset, d => d.variance);
     const tempMin = d3.min(dataset, d => d.variance);
-    const cellWidth = (width - padding)/(yearMax - yearMin);
-    const cellHeight = (height -padding)/12;
+    const cellWidth = (width)/(yearMax - yearMin);
+    const cellHeight = (height)/12;
     
     const svg = d3.select(root)
                     .append('svg')
-                    .attr('width', width)
-                    .attr('height', height);
+                    .attr('width', width + padding.right + padding.left)
+                    .attr('height', height + padding.top + padding.bottom)
+                    .append('g')
+                    .attr('transform', `translate(${padding.left}, ${padding.top})`)
     // Titles
     svg.append('text')
         .text('Monthly Global Land-Surface Temperature')
         .attr('text-anchor', 'middle')
         .attr('id', 'title')
         .attr('font-size', '20')
-        .attr('transform', `translate(${width/2}, 50)`);
+        .attr('transform', `translate(${width/2}, -50)`);
     svg.append('text')
         .text('1753 - 2015: base temperature 8.66℃')
         .attr('id', 'description')
         .attr('text-anchor', 'middle')
-        .attr('transform', `translate(${width/2}, 75)`);
+        .attr('transform', `translate(${width/2}, -25)`);
 
     // x-scale
     const parseTimeYear = d3.timeParse('%Y');
     const xScaleYear = d3.scaleTime()
                             .domain([parseTimeYear(yearMin), parseTimeYear(yearMax)])
-                            .range([padding, width-padding])
+                            .range([0, width])
     const xAxis = d3.axisBottom(xScaleYear).tickFormat(d3.timeFormat('%Y'))
                     .tickArguments([d3.timeYear.every(10)]);
 
@@ -52,20 +55,18 @@ const buildGraph = (data) => {
    
     const yScaleMonth = d3.scaleBand()
                 .domain([1,2,3,4,5,6,7,8,9,10,11,12])
-                .range([padding,height - padding])
+                .range([0,height])
     
     const yAxis = d3.axisLeft(yScaleMonth)                
                     .tickFormat(formatMonth);
-
-    console.log(yScaleMonth(0));
                     
     // Axes
     svg.append('g')
-        .attr('transform', `translate(0,${height - padding})`)
+        .attr('transform', `translate(0,${height})`)
         .property('id', 'x-axis')
         .call(xAxis);
     svg.append('g')
-        .attr('transform', `translate(${padding}, 0)`)
+        .attr('transform', `translate(${0}, ${0})`)
         .property('id', 'y-axis')
         .call(yAxis);
 
